dde35481ab7c3023a6d005d6991c282d073b0594
fanzenghui <fanzenghui@baidu-mgame.com>
2021-03-31 15:24:31 +0800

    190. 颠倒二进制位 II，74. 搜索二维矩阵

---
+++ src/daily/DAY202103310090.java
@@ -0,0 +1,54 @@
+package daily;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 90. 子集 II
+ * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ *
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,2]
+ * 输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+ * 示例 2：
+ *
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ */
+public class DAY202103310090 {
+
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        dfs(false, 0, nums);
+        return ans;
+    }
+
+    public void dfs(boolean choosePre, int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        dfs(false, cur + 1, nums);
+        if (!choosePre && cur > 0 && nums[cur - 1] == nums[cur]) {
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(true, cur + 1, nums);
+        t.remove(t.size() - 1);
+    }
+}
