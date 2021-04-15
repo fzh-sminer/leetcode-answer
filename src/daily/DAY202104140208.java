cdd6d34d26bdc54950bb4f5ea41dc80b5fdaa966
fanzenghui <fanzenghui@baidu-mgame.com>
2021-04-15 17:21:49 +0800

    208. 实现 Trie (前缀树)，213. 打家劫舍 II

---
+++ src/daily/DAY202104140208.java
@@ -0,0 +1,117 @@
+package daily;
+
+/**
+ * 208. 实现 Trie (前缀树)
+ * Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+ *
+ * 请你实现 Trie 类：
+ *
+ * Trie() 初始化前缀树对象。
+ * void insert(String word) 向前缀树中插入字符串 word 。
+ * boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+ * boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+ *
+ *
+ * 示例：
+ *
+ * 输入
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * 输出
+ * [null, null, true, false, true, null, true]
+ *
+ * 解释
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 True
+ * trie.search("app");     // 返回 False
+ * trie.startsWith("app"); // 返回 True
+ * trie.insert("app");
+ * trie.search("app");     // 返回 True
+ *
+ *
+ * 提示：
+ *
+ * 1 <= word.length, prefix.length <= 2000
+ * word 和 prefix 仅由小写英文字母组成
+ * insert、search 和 startsWith 调用次数 总计 不超过 3 * 104 次
+ */
+public class DAY202104140208 {
+
+    static class Trie {
+        private TreeNode root;
+
+        /** Initialize your data structure here. */
+        public Trie() {
+            root = new TreeNode(new TreeNode[26], false);
+        }
+
+        /** Inserts a word into the trie. */
+        public void insert(String word) {
+            TreeNode node = root;
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null) {
+                    node.children[idx] = new TreeNode(new TreeNode[26], false);
+                }
+                node = node.children[idx];
+            }
+            node.isEnd = true;
+        }
+
+        /** Returns if the word is in the trie. */
+        public boolean search(String word) {
+            TreeNode node = root;
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null) {
+                    return false;
+                }
+                node = node.children[idx];
+            }
+            return node.isEnd;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public boolean startsWith(String prefix) {
+            TreeNode node = root;
+            for (char c : prefix.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null) {
+                    return false;
+                }
+                node = node.children[idx];
+            }
+            return true;
+        }
+
+
+        private static class TreeNode {
+            TreeNode[] children;
+            boolean isEnd;
+            public TreeNode() {
+            }
+            public TreeNode(TreeNode[] children, boolean isEnd) {
+                this.children = children;
+                this.isEnd = isEnd;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new DAY202104140208.Trie();
+        trie.insert("apple");
+        boolean isEnd = trie.search("apple");
+        System.out.println(isEnd);
+        boolean isPre = trie.startsWith("appe");
+        System.out.println(isPre);
+    }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+}
