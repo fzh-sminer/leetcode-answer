07bfa8827cbc9e7b614f88d2e999ee1db6cc18e9
fanzenghui <fanzenghui@baidu-mgame.com>
2021-04-28 10:18:17 +0800

    363. 矩形区域不超过 K 的最大数值和，1011. 在 D 天内送达包裹的能力，938. 二叉搜索树的范围和，633. 平方数之和

---
+++ src/daily/DAY202104270938.java
@@ -0,0 +1,95 @@
+package daily;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 938. 二叉搜索树的范围和
+ * 给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+ *
+ *
+ *
+ * 示例 1：
+ * https://assets.leetcode.com/uploads/2020/11/05/bst1.jpg
+ *
+ * 输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+ * 输出：32
+ *
+ * 示例 2：
+ * https://assets.leetcode.com/uploads/2020/11/05/bst2.jpg
+ *
+ * 输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+ * 输出：23
+ *
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [1, 2 * 104] 内
+ * 1 <= Node.val <= 105
+ * 1 <= low <= high <= 105
+ * 所有 Node.val 互不相同
+ */
+public class DAY202104270938 {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        return first(root, low, high);
+    }
+
+    public int first(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+
+    public int second(TreeNode root, int low, int high) {
+        int sum = 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                continue;
+            }
+            if (node.val > high) {
+                q.offer(node.left);
+            } else if (node.val < low) {
+                q.offer(node.right);
+            } else {
+                sum += node.val;
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+        return sum;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.left.left= new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        root.right.right = new TreeNode(18);
+
+        System.out.println(new DAY202104270938().rangeSumBST(root, 7, 15));
+    }
+}
