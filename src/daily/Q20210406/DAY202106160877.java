78f8ed59283f6d91da0f1c295263002fa8ae7685
fanzenghui <fanzenghui@baidu-mgame.com>
2021-06-17 14:34:32 +0800

    many commit

---
+++ src/daily/Q20210406/DAY202106160877.java
@@ -0,0 +1,18 @@
+package daily.Q20210406;
+
+public class DAY202106160877 {
+
+    public boolean stoneGame(int[] piles) {
+        int length = piles.length;
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] > 0;
+    }
+}
